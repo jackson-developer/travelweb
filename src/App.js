42594cc4ba9ea3fc03eb3c9d455cc905f21d3cf7
aleksandr <aleksandr600000@outlook.com>
2021-01-07 07:33:59 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import './App.css';
+
+import Productdetail from "./component/product/Productdetail";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container-fluid">
+        <Productdetail />
+      </div>
+    </div>
+  );
+}
+
+export default App;
